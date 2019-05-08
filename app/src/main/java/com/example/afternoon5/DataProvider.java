c57b7f694bf2e19cc5c5ad99fe3aaa4aa210774e
Philipp Eisner <philipp.eisner@student.tugraz.at>
2019-05-08 16:55:50 +0200

    [EP, GM] added menu bar to export notes

--- app/src/main/java/com/example/afternoon5/DataProvider.java
+++ app/src/main/java/com/example/afternoon5/DataProvider.java
@@ -2,11 +2,15 @@ package com.example.afternoon5;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Environment;
+import android.util.Log;
 
 import com.example.afternoon5.HelperClasses.Note;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 class DataProvider {
@@ -89,6 +93,40 @@ class DataProvider {
             }
         }
         return tags;
+    }
+
+    public void exportToExternalStorage(ArrayList<Note> notes)
+    {
+        File dir  = new File(Environment.getExternalStorageDirectory(), "NoteExport");
+        if (!dir.mkdirs()) {
+            Log.e("DATA", "Directory not created");
+        }
+        for(Note note : notes)
+        {
+            File file = noteToFile(note, dir);
+
+        }
+
+
+    }
+    private File noteToFile(Note note, File path)
+    {
+        File file = new File(path, Integer.toString(note.hashCode()));
+        try {
+
+        FileOutputStream stream = new FileOutputStream(file);
+        try {
+            Gson gson = new Gson();
+            stream.write(gson.toJson(note).getBytes());
+        } finally {
+            stream.close();
+        }}
+        catch (Exception e)
+        {
+
+        }
+        return file;
+
     }
 
 
--- app/src/main/java/com/example/afternoon5/MainActivity.java
+++ app/src/main/java/com/example/afternoon5/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.afternoon5;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Environment;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 
@@ -14,14 +18,26 @@ import android.widget.AdapterView;
 
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CheckBox;
 import android.widget.ListView;
 
 import com.example.afternoon5.HelperClasses.Note;
 import com.example.afternoon5.HelperClasses.list_adapter;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Menu menu;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        this.menu = menu;
+        return true;
+    }
 
 
     @Override
@@ -49,9 +65,15 @@ public class MainActivity extends AppCompatActivity {
                 {
                     // Log.i("MAIN", "DEBUG: " + list.getAdapter().get);
                     // list.getAdapter().getView(i, parent, null).findViewById(R.id.export_checkbox).setVisibility(View.VISIBLE);
+
                     View view1  = list.getChildAt(i);
                     view1.findViewById(R.id.export_checkbox).setVisibility(View.VISIBLE);
+                    //findViewById(R.id.action_export).setVisibility(View.VISIBLE);
+
+
                 }
+                menu.findItem(R.id.action_export).setVisible(true);
+                menu.findItem(R.id.action_cancel).setVisible(true);
                 return true;
             }
         });
@@ -71,6 +93,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void closeExport(MenuItem menuItem) {
+        final ListView list = (ListView) findViewById(R.id.node_list);
+        for (int i = 0; i < list.getCount(); i++)
+        {
+            // Log.i("MAIN", "DEBUG: " + list.getAdapter().get);
+            // list.getAdapter().getView(i, parent, null).findViewById(R.id.export_checkbox).setVisibility(View.VISIBLE);
+
+            View view1  = list.getChildAt(i);
+            view1.findViewById(R.id.export_checkbox).setVisibility(View.GONE);
+            //findViewById(R.id.action_export).setVisibility(View.VISIBLE);
+
+
+        }
+        menu.findItem(R.id.action_export).setVisible(false);
+        menu.findItem(R.id.action_cancel).setVisible(false);
+
+    }
+    public void exportSelectedNotes(MenuItem menuItem) {
+        final ListView list = (ListView) findViewById(R.id.node_list);
+        ArrayList<Note> listtoExport = new ArrayList<>();
+        for (int i = 0; i < list.getCount(); i++)
+        {
+            // Log.i("MAIN", "DEBUG: " + list.getAdapter().get);
+            // list.getAdapter().getView(i, parent, null).findViewById(R.id.export_checkbox).setVisibility(View.VISIBLE);
+
+            View view1  = list.getChildAt(i);
+            if (((CheckBox)view1.findViewById(R.id.export_checkbox)).isChecked())
+                listtoExport.add(DataProvider.getInstance().getNotes().get(i));
+        }
+        if(isExternalStorageWritable())
+        {
+
+        }
+
+
+    }
 
 
     public void openCreateNote(View view) {
@@ -84,6 +142,14 @@ public class MainActivity extends AppCompatActivity {
     final ListView list = findViewById(R.id.node_list);
     final list_adapter adapter = new list_adapter(this,DataProvider.getInstance().getNotes());
     list.setAdapter(adapter);
+    }
+
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
     }
 
 }

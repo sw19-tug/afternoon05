fb4b60c3b4bbef61a84a7e4aaaca4702cfb0e178
Julia Fuchs <j.fuchs@student.tugraz.at>
2019-06-12 14:08:58 +0200

    NOTE_017 Changed colorpicker icon [GM,FJ]

--- app/src/main/java/com/example/afternoon5/CreateNoteActivity.java
+++ app/src/main/java/com/example/afternoon5/CreateNoteActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -83,5 +85,11 @@ public class CreateNoteActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.create_menu, menu);
+        return true;
     }
 }
